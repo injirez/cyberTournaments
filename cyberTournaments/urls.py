a93d568df2a00053555dcb3f61476df8dd05be29
injirka <injirka@outlook.com>
2021-07-12 22:03:23 +0300

    Fix static

--- cyberTournaments/urls.py
+++ cyberTournaments/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from .yasg import urlpatterns as yasgUrls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('Dota/', include('Dota.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += yasgUrls
