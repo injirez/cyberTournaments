f055d0bce2c93616b1129c7ed7a8ad3e54ea41c5
injirka <injirka@outlook.com>
2021-07-12 21:25:57 +0300

    ughb

--- cyberTournaments/settings.py
+++ cyberTournaments/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '2qba_+8k*m9gjhft7&+=un!sid#b@vd*2#xv1s0k^yw26i4134'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.8', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
