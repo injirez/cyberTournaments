450ce0f11a5e18885f7b92eff8f08ad13a59ddaf
injirka <injirka@outlook.com>
2021-07-12 21:55:33 +0300

    Fix setting.py

--- cyberTournaments/settings.py
+++ cyberTournaments/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 
 # REST_FRAMEWORK = {
 #     #this bit makes the magic.
